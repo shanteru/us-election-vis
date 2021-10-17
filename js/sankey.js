8123e517b60611fbcbb1e19aebe467f0a89d9fef
cloh0007 <cloh0007@student.monash.edu>
2021-10-17 13:22:26 +0800

    vis2 added many chart

---
+++ js/sankey.js
@@ -0,0 +1,121 @@
+
+// initialize the echarts instance
+
+var myChart = echarts.init(document.getElementById("sankey"));
+var option;
+
+// Draw the chart
+
+const data = {
+    nodes: [
+        { name: "Total" },
+        { name: "Female" },
+        { name: "Male" },
+        { name: "White" },
+        { name: "Black" },
+        { name: "Latino" },
+        { name: "AAPI" },
+        { name: "Other" },
+        { name: "Non-College" },
+        { name: "College" }
+    ],
+    links: [
+        { source: "Total", target: "Female", value: 5.4 },
+        { source: "Total", target: "Male", value: 4.6 },
+        { source: "Female", target: "Non-College", value: 3.2 },
+        {
+            source: "Female",
+            target: "College",
+            value: 2.0,
+        },
+        {
+            source: "Male",
+            target: "Non-College",
+            value: 2.9,
+        },
+        {
+            source: "Male",
+            target: "College",
+            value: 1.7,
+        },
+
+        { source: "Non-College", target: "White", value: 4.4 },
+        { source: "College", target: "White", value: 2.8},
+        { source: "Non-College", target: "Black", value: 0.8 },
+        { source: "Non-College", target: "Latino", value: 0.6 },
+        { source: "Non-College", target: "AAPI", value: 0.2 },
+        { source: "Non-College", target: "Other", value: 0.1 },
+
+        { source: "College", target: "Black", value: 0.4 },
+        { source: "College", target: "Latino", value: 0.3 },
+        { source: "College", target: "AAPI", value: 0.17 },
+        { source: "College", target: "Other", value: 0.03 },
+        
+    ],
+};
+
+myChart.setOption(
+    (option = {
+        tooltip: {
+            trigger: "item",
+            triggerOn: "mousemove",
+        },
+        series: [
+            {
+                type: "sankey",
+                data: data.nodes,
+                links: data.links,
+                emphasis: {
+                    focus: "adjacency",
+                },
+                levels: [
+                    {
+                        depth: 0,
+                        itemStyle: {
+                            color: "#ed553b",
+                        },
+                        lineStyle: {
+                            color: "source",
+                            opacity: 0.6,
+                        },
+                    },
+                    {
+                        depth: 1,
+                        itemStyle: {
+                            color: "#3caea3",
+                        },
+                        lineStyle: {
+                            color: "source",
+                            opacity: 0.6,
+                        },
+                    },
+                    {
+                        depth: 2,
+                        itemStyle: {
+                            color: "#20639b",
+                        },
+                        lineStyle: {
+                            color: "source",
+                            opacity: 0.6,
+                        },
+                    },
+                    {
+                        depth: 3,
+                        itemStyle: {
+                            color: "#173f5f",
+                        },
+                        lineStyle: {
+                            color: "source",
+                            opacity: 0.6,
+                        },
+                    },
+                ],
+                lineStyle: {
+                    curveness: 0.5,
+                },
+            },
+        ],
+    })
+);
+
+option && myChart.setOption(option);
